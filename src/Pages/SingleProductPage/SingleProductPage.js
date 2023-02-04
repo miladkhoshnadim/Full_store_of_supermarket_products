1394bfd4af0eaf782034c866d5bc83a7a425fe01
milad <miladkhoshnadim1372@gmail.com>
2023-02-04 15:28:38 -0800

    Page single Product reload Completed

--- src/Pages/SingleProductPage/SingleProductPage.js
+++ src/Pages/SingleProductPage/SingleProductPage.js
@@ -1,9 +1,11 @@
 import SingleProductStyle from "./SingleProductPage.module.css";
 import { useParams } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { contexInfo } from "../../Component/context/Contex";
+import { ServiceProducts } from "../../services/Servise";
 
 export const SingleProductPage = () => {
+  const [Product, setProduct] = useState({});
   const { ProductId } = useParams();
   const addresOneProduct = ProductId.split("ID");
   const InformationContext = useContext(contexInfo);
@@ -11,48 +13,62 @@ export const SingleProductPage = () => {
     (x) => x === addresOneProduct[0]
   );
 
-  console.log("InformationContext", InformationContext);
-
-  const elem = InformationContext.dataGrops[index].find(
-    (el) => el.id == addresOneProduct[1]
-  );
-
-  //Grop[0].CategoryFarsi
+  useEffect(() => {
+    if (index > -1) {
+      const Productt = InformationContext.dataGrops[index].find(
+        (el) => el.id == addresOneProduct[1]
+      );
+      setProduct(Productt);
+    } else {
+      fetch(ServiceProducts + `/Products/${addresOneProduct[1]}`)
+        .then((res) => res.json())
+        .then((result) => setProduct(result));
+    }
+  }, []);
 
   return (
     <div className={SingleProductStyle.infinityDivDisplay}>
       {" "}
       <div className={SingleProductStyle.MainDivDisplay}>
-        <div className={SingleProductStyle.SubjectPage}>
-          <span>
-            محصولی از دسته بندی {elem.CategoryFarsi} برند {elem.brand}
-          </span>
-        </div>
-        <div className={SingleProductStyle.MainSingleProductDiv}>
-          <div className={SingleProductStyle.ImgSubjectProductDiv}>
-            <div className={SingleProductStyle.DivImg}>
-              <img className={SingleProductStyle.ImgProduct} src={elem.img} />
+        {Product.id > -1 ? (
+          <>
+            <div className={SingleProductStyle.SubjectPage}>
+              <span>
+                محصولی از دسته بندی {Product.CategoryFarsi} برند {Product.brand}
+              </span>
             </div>
+            <div className={SingleProductStyle.MainSingleProductDiv}>
+              <div className={SingleProductStyle.ImgSubjectProductDiv}>
+                <div className={SingleProductStyle.DivImg}>
+                  <img
+                    className={SingleProductStyle.ImgProduct}
+                    src={Product.img}
+                  />
+                </div>
 
-            <div className={SingleProductStyle.ExplainProductDiv}>
-              <span className={SingleProductStyle.LabelProduct}>
-                {elem.Lable}
-              </span>
-              <span className={SingleProductStyle.PriceProduct}>
-                {" "}
-                {elem.price} ریال{" "}
-              </span>
-              <div className={SingleProductStyle.CounterDiv}>
-                <span>+</span>
-                <span className={SingleProductStyle.CounterNumber}>0</span>
-                <span className={SingleProductStyle.MinesSpan}>_</span>
+                <div className={SingleProductStyle.ExplainProductDiv}>
+                  <span className={SingleProductStyle.LabelProduct}>
+                    {Product.Lable}
+                  </span>
+                  <span className={SingleProductStyle.PriceProduct}>
+                    {" "}
+                    {Product.price} ریال{" "}
+                  </span>
+                  <div className={SingleProductStyle.CounterDiv}>
+                    <span>+</span>
+                    <span className={SingleProductStyle.CounterNumber}>0</span>
+                    <span className={SingleProductStyle.MinesSpan}>_</span>
+                  </div>
+                </div>
               </div>
+              <p className={SingleProductStyle.ExplainProductExactly}>
+                {Product.explain}
+              </p>
             </div>
-          </div>
-          <p className={SingleProductStyle.ExplainProductExactly}>
-            {elem.explain}
-          </p>
-        </div>
+          </>
+        ) : (
+          <div>در حال بارگذاری...</div>
+        )}
       </div>{" "}
     </div>
   );
